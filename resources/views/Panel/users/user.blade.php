e69cf0b6dd12e227bdb8e53b454496ef14ca4e0f
FajarSidiq08 <sidiqfajar491@gmail.com>
2025-04-25 14:36:54 +0700

    User V1 : Edit Users Design #2

--- resources/views/Panel/users/user.blade.php
+++ resources/views/Panel/users/user.blade.php
@@ -1,36 +1,34 @@
 @extends('layout.template')
 @section('main')
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <a href="#"
-            class="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            class="block p-6 bg-[#57B4BA] text-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight">
                 Departemen
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
-                Penjelasan
+            <p class="font-normal">
+                Kelola data departemen
             </p>
         </a>
 
         <a href="#"
-            class="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            class="block p-6 bg-[#57B4BA] text-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight">
                 Guru
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
-                Penjelasan
+            <p class="font-normal">
+                Kelola data guru
             </p>
         </a>
 
         <a href="#"
-            class="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            class="block p-6 bg-[#57B4BA] text-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight">
                 Siswa
             </h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">
-                Penjelasan
+            <p class="font-normal">
+                Kelola data siswa
             </p>
         </a>
-
-
     </div>
 @endsection
