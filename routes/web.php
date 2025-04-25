1f451d17c36a9f2670d06c626b50037c67cc0652
Mosses Aryo Bimo <mossesaryobimo@gmail.com>
2025-04-25 15:09:10 +0700

    Add Routing

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Panel.dashboard');
 });
 
-
 Route::get('/users', function() {
     return view('Panel.users.user');
+});
+
+Route::get('/informasi', function() {
+    return view('Panel.informasi.informasi');
 });
