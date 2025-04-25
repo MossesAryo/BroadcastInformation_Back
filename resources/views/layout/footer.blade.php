193c5a4d9940b38cb38b90eae619184cd13e37a7
Mosses Aryo Bimo <mossesaryobimo@gmail.com>
2025-04-25 13:24:44 +0700

    Dashboard V1 : Add Templating Engine Logic #2

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-white border-t border-gray-200 mt-auto">
+    <div class="max-w-7xl mx-auto py-4 px-4 text-center">
+        <p class="text-sm text-gray-500">
+            &copy; Sebelas Informasi. All rights reserved.
+        </p>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layout.template');
+    return view('Panel.dashboard');
 });
