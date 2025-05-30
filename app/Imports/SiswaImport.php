be0329da2f7ddc61532315e25cb07aa6125ef3cc
nadhiipp <muhammadnadiprahmatih@gmail.com>
2025-05-30 22:48:00 +0700

    Adding Import Guru-Siswa

---
+++ app/Imports/SiswaImport.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Siswa;
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Log;
+
+class SiswaImport implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+   
+
+        $email = $row['email'];
+
+        // Buat user baru jika belum ada
+        $user = User::firstOrCreate([
+            'username' => $email,
+        ], [
+            'email' => $email,
+            'password' => bcrypt('password'), 
+        ]);
+
+        return new Siswa([
+            'ID_Siswa'   => $row['nis'],
+            'Nama_Siswa' => $row['nama_siswa'],
+            'username'   => $email,
+        ]);
+    }
+}
