56a6cfc4b6629af59d247ab50b18050c32241ee2
Mosses Aryo Bimo <mossesaryobimo@gmail.com>
2025-06-13 08:18:46 +0700

    Fixing bugs

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Admin Dashboard</title>
+    <title>EduInform</title>
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Alpine.js for interactions -->
